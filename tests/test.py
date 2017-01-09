04174913313b9acd0a6acf538d50122b0f84a022
Susanne <susanneTsauer@gmx.net>
2017-01-09 22:00:00 +0100

    add file

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+﻿import test_ownfunctions
+import test_geocache
+import test_user_io
+
+verbosity = input("Verbosity (1 oder 2): ")
+
+test_ownfunctions.main(verbosity)
+test_geocache.main(verbosity)
+test_user_io.main(verbosity)
